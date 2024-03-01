7e7a362bb5bfdb8b3d8733c35115b8d9f8334686
Jun Kai Law <junkailaw1533@gmail.com>
2024-03-01 02:30:13 +0000

    Update remove functionality

--- Build/score_web_build.framework.js
+++ Build/score_web_build.framework.js
@@ -1976,13 +1976,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  5253244: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 5253299: function($0) {performance.now = function() { return $0; };},  
- 5253347: function($0) {performance.now = function() { return $0; };},  
- 5253395: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 5253450: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 5253511: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 5253575: function() {return Module.webglContextAttributes.powerPreference;}
+  5253276: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 5253331: function($0) {performance.now = function() { return $0; };},  
+ 5253379: function($0) {performance.now = function() { return $0; };},  
+ 5253427: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 5253482: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 5253543: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 5253607: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
