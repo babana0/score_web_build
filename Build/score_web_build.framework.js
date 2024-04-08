ceeec86511b00d958bd2fcce29c1e175474f4ee2
Jun Kai Law <junkailaw1533@gmail.com>
2024-04-08 03:52:52 +0100

    update

--- Build/score_web_build.framework.js
+++ Build/score_web_build.framework.js
@@ -1976,13 +1976,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4672844: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4672899: function($0) {performance.now = function() { return $0; };},  
- 4672947: function($0) {performance.now = function() { return $0; };},  
- 4672995: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 4673050: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4673111: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4673175: function() {return Module.webglContextAttributes.powerPreference;}
+  4672812: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4672867: function($0) {performance.now = function() { return $0; };},  
+ 4672915: function($0) {performance.now = function() { return $0; };},  
+ 4672963: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 4673018: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4673079: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4673143: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
